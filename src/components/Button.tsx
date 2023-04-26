c9a72f046e8e29f98242c3501c381136712062d2
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-04-26 12:00:00 +0200

    Add button

---
+++ src/components/Button.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+  variant: string;
+}
+
+export default function Button({ children, variant, ...props }: ButtonProps) {
+  return (
+    <>
+      <button
+        className={`rounded-lg px-5 py-2 text-xl border-[1.5px] outline-none transition-all ${
+          variant === "primary"
+            ? "bg-[#435cfa] border-[#435cfa] shadow-xl text-slate-100 focus:ring-2"
+            : ""
+        } ${
+          variant === "outline"
+            ? "border-zinc-600 bg-zinc-100 text-zinc-600"
+            : ""
+        }`}
+        {...props}
+      >
+        {children}
+      </button>
+    </>
+  );
+}
